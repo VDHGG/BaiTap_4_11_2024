b0c653c001fd545c7e0f575f6505b9b3a295ad2a
VDHGG <vuduchuy2072004@gmail.com>
2024-11-04 14:22:41 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace BaiTap4_11_2024
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TenKhText = new System.Windows.Forms.TextBox();
+            this.SDTText = new System.Windows.Forms.TextBox();
+            this.MaKhText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuaButton = new System.Windows.Forms.Button();
+            this.XoaButton = new System.Windows.Forms.Button();
+            this.ThemButton = new System.Windows.Forms.Button();
+            this.dgvKhachHang = new System.Windows.Forms.DataGridView();
+            this.DiaChiKhText = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dgvDichVu = new System.Windows.Forms.DataGridView();
+            this.ThanhToanButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKhachHang)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDichVu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TenKhText
+            // 
+            this.TenKhText.Location = new System.Drawing.Point(492, 283);
+            this.TenKhText.Name = "TenKhText";
+            this.TenKhText.Size = new System.Drawing.Size(326, 22);
+            this.TenKhText.TabIndex = 20;
+            // 
+            // SDTText
+            // 
+            this.SDTText.Location = new System.Drawing.Point(492, 334);
+            this.SDTText.Name = "SDTText";
+            this.SDTText.Size = new System.Drawing.Size(326, 22);
+            this.SDTText.TabIndex = 19;
+            // 
+            // MaKhText
+            // 
+            this.MaKhText.Location = new System.Drawing.Point(492, 237);
+            this.MaKhText.Name = "MaKhText";
+            this.MaKhText.Size = new System.Drawing.Size(326, 22);
+            this.MaKhText.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(368, 340);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 16);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "NHẬP SDT KH:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(368, 289);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 16);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "NHẬP TÊN KH:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(368, 244);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 16);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "NHẬP MKH:";
+            // 
+            // SuaButton
+            // 
+            this.SuaButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SuaButton.Location = new System.Drawing.Point(483, 441);
+            this.SuaButton.Name = "SuaButton";
+            this.SuaButton.Size = new System.Drawing.Size(96, 32);
+            this.SuaButton.TabIndex = 14;
+            this.SuaButton.Text = "SỬA";
+            this.SuaButton.UseVisualStyleBackColor = true;
+            this.SuaButton.Click += new System.EventHandler(this.SuaButton_Click);
+            // 
+            // XoaButton
+            // 
+            this.XoaButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.XoaButton.Location = new System.Drawing.Point(606, 441);
+            this.XoaButton.Name = "XoaButton";
+            this.XoaButton.Size = new System.Drawing.Size(96, 32);
+            this.XoaButton.TabIndex = 13;
+            this.XoaButton.Text = "XÓA";
+            this.XoaButton.UseVisualStyleBackColor = true;
+            this.XoaButton.Click += new System.EventHandler(this.XoaButton_Click);
+            // 
+            // ThemButton
+            // 
+            this.ThemButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ThemButton.Location = new System.Drawing.Point(370, 441);
+            this.ThemButton.Name = "ThemButton";
+            this.ThemButton.Size = new System.Drawing.Size(96, 32);
+            this.ThemButton.TabIndex = 12;
+            this.ThemButton.Text = "THÊM";
+            this.ThemButton.UseVisualStyleBackColor = true;
+            this.ThemButton.Click += new System.EventHandler(this.ThemButton_Click);
+            // 
+            // dgvKhachHang
+            // 
+            this.dgvKhachHang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKhachHang.Location = new System.Drawing.Point(42, 42);
+            this.dgvKhachHang.Name = "dgvKhachHang";
+            this.dgvKhachHang.RowHeadersWidth = 51;
+            this.dgvKhachHang.RowTemplate.Height = 24;
+            this.dgvKhachHang.Size = new System.Drawing.Size(631, 159);
+            this.dgvKhachHang.TabIndex = 11;
+            // 
+            // DiaChiKhText
+            // 
+            this.DiaChiKhText.Location = new System.Drawing.Point(492, 392);
+            this.DiaChiKhText.Name = "DiaChiKhText";
+            this.DiaChiKhText.Size = new System.Drawing.Size(326, 22);
+            this.DiaChiKhText.TabIndex = 21;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(367, 398);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 16);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "NHẬP ĐỊA CHỈ KH :";
+            // 
+            // dgvDichVu
+            // 
+            this.dgvDichVu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDichVu.Location = new System.Drawing.Point(700, 42);
+            this.dgvDichVu.Name = "dgvDichVu";
+            this.dgvDichVu.RowHeadersWidth = 51;
+            this.dgvDichVu.RowTemplate.Height = 24;
+            this.dgvDichVu.Size = new System.Drawing.Size(496, 159);
+            this.dgvDichVu.TabIndex = 23;
+            // 
+            // ThanhToanButton
+            // 
+            this.ThanhToanButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ThanhToanButton.Location = new System.Drawing.Point(722, 441);
+            this.ThanhToanButton.Name = "ThanhToanButton";
+            this.ThanhToanButton.Size = new System.Drawing.Size(171, 32);
+            this.ThanhToanButton.TabIndex = 24;
+            this.ThanhToanButton.Text = "THANH TOÁN";
+            this.ThanhToanButton.UseVisualStyleBackColor = true;
+            this.ThanhToanButton.Click += new System.EventHandler(this.ThanhToanButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1390, 485);
+            this.Controls.Add(this.ThanhToanButton);
+            this.Controls.Add(this.dgvDichVu);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.DiaChiKhText);
+            this.Controls.Add(this.TenKhText);
+            this.Controls.Add(this.SDTText);
+            this.Controls.Add(this.MaKhText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SuaButton);
+            this.Controls.Add(this.XoaButton);
+            this.Controls.Add(this.ThemButton);
+            this.Controls.Add(this.dgvKhachHang);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKhachHang)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDichVu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TenKhText;
+        private System.Windows.Forms.TextBox SDTText;
+        private System.Windows.Forms.TextBox MaKhText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button SuaButton;
+        private System.Windows.Forms.Button XoaButton;
+        private System.Windows.Forms.Button ThemButton;
+        private System.Windows.Forms.DataGridView dgvKhachHang;
+        private System.Windows.Forms.TextBox DiaChiKhText;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dgvDichVu;
+        private System.Windows.Forms.Button ThanhToanButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTap4_11_2024
+{
+    public partial class Form1 : Form
+    {
+        public class KhachHang
+        {
+            public string MaKhachHang { get; set; }
+            public string TenKhachHang { get; set; }
+            public string sdt { get; set; }
+            public string DiaChi { get; set; }  
+        }
+
+        public class DichVu
+        {
+            public string MaDichVu { get; set; }
+            public string TenDichVu { get; set; }
+            public string GiaTien { get; set; }
+        }
+
+        private List<KhachHang> DanhSachKhachHang = new List<KhachHang>();
+        private List<DichVu> DanhSachDichVu = new List<DichVu>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            dgvKhachHang.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvKhachHang.MultiSelect = false;      
+            dgvDichVu.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+        }
+
+        private void CapNhatDataGridView()
+        {
+            dgvKhachHang.DataSource = null;
+            dgvKhachHang.DataSource = DanhSachKhachHang;
+            dgvDichVu.DataSource = null;
+            dgvDichVu.DataSource = DanhSachDichVu;
+        }
+
+        private void ClearTextBoxes()
+        {
+            MaKhText.Clear();
+            TenKhText.Clear();
+            SDTText.Clear();
+            DiaChiKhText.Clear();     
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DanhSachKhachHang = new List<KhachHang>
+            {
+                new KhachHang { MaKhachHang = "KH01", TenKhachHang = "Nguyen Van A", sdt = "123456789", DiaChi = "123 Đường ABC, Hà Nội" },
+                new KhachHang { MaKhachHang = "KH02", TenKhachHang = "Le Thi B", sdt = "987654321", DiaChi = "456 Đường DEF, Hà Nội" },
+                new KhachHang { MaKhachHang = "KH03", TenKhachHang = "Tran Van C", sdt = "112233445", DiaChi = "789 Đường GHI, TP.HCM" },
+                new KhachHang { MaKhachHang = "KH04", TenKhachHang = "Pham Thi D", sdt = "223344556", DiaChi = "101 Đường JKL, Đà Nẵng" },
+                new KhachHang { MaKhachHang = "KH05", TenKhachHang = "Hoang Van E", sdt = "334455667", DiaChi = "202 Đường MNO, Cần Thơ" },
+                new KhachHang { MaKhachHang = "KH06", TenKhachHang = "Vo Thi F", sdt = "445566778", DiaChi = "303 Đường PQR, Nha Trang" },
+                new KhachHang { MaKhachHang = "KH07", TenKhachHang = "Phan Van G", sdt = "556677889", DiaChi = "404 Đường STU, Hải Phòng" },
+                new KhachHang { MaKhachHang = "KH08", TenKhachHang = "Do Thi H", sdt = "667788990", DiaChi = "505 Đường VWX, Huế" }
+            };
+
+            DanhSachDichVu = new List<DichVu>
+            {
+              new DichVu { MaDichVu = "DV01", TenDichVu = "Dịch vụ vệ sinh máy lạnh", GiaTien = "200,000$" },
+              new DichVu { MaDichVu = "DV02", TenDichVu = "Sửa chữa máy tính", GiaTien = "500,000$" },
+              new DichVu { MaDichVu = "DV03", TenDichVu = "Thay thế màn hình điện thoại", GiaTien = "700,000$" },
+              new DichVu { MaDichVu = "DV04", TenDichVu = "Nâng cấp phần mềm", GiaTien = "150,000$" },
+              new DichVu { MaDichVu = "DV05", TenDichVu = "Dịch vụ vệ sinh nhà cửa", GiaTien = "300,000$" }
+             };
+
+            CapNhatDataGridView();
+        }
+
+
+
+
+        private void ThemButton_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(MaKhText.Text) && !string.IsNullOrEmpty(TenKhText.Text) && !string.IsNullOrEmpty(SDTText.Text) && !string.IsNullOrEmpty(DiaChiKhText.Text))
+            {
+                KhachHang kh = new KhachHang()
+                {
+                    MaKhachHang = MaKhText.Text,
+                    TenKhachHang = TenKhText.Text,
+                    sdt = SDTText.Text,
+                    DiaChi = DiaChiKhText.Text,
+                };
+
+                DanhSachKhachHang.Add(kh);
+                CapNhatDataGridView();
+                ClearTextBoxes();
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng điền đầy đủ thông tin khách hàng.");
+            }
+
+
+        }
+
+        private void SuaButton_Click(object sender, EventArgs e)
+        {
+            if (dgvKhachHang.SelectedRows.Count > 0)
+            {
+                int index = dgvKhachHang.SelectedRows[0].Index;
+                KhachHang kh = DanhSachKhachHang[index];
+
+                kh.MaKhachHang = MaKhText.Text;
+                kh.TenKhachHang = TenKhText.Text;
+                kh.sdt = SDTText.Text;
+                kh.DiaChi = DiaChiKhText.Text;
+
+                CapNhatDataGridView();
+                ClearTextBoxes();
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn khách hàng để sửa.");
+            }
+        }
+
+        private void XoaButton_Click(object sender, EventArgs e)
+        {
+            if (dgvKhachHang.SelectedRows.Count > 0)
+            {
+                int index = dgvKhachHang.SelectedRows[0].Index;
+                DanhSachKhachHang.RemoveAt(index);
+
+                CapNhatDataGridView();
+                ClearTextBoxes();
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn khách hàng muốn xóa.");
+            }
+
+        }
+
+        private void ThanhToanButton_Click(object sender, EventArgs e)
+        {
+            if (dgvKhachHang.SelectedRows.Count > 0 && dgvDichVu.SelectedRows.Count > 0)
+            {
+               
+                int khachHangIndex = dgvKhachHang.SelectedRows[0].Index;
+                KhachHang khachHang = DanhSachKhachHang[khachHangIndex];
+
+                
+                List<DichVu> dichVuDaChon = new List<DichVu>();
+                decimal tongTien = 0;
+
+                foreach (DataGridViewRow row in dgvDichVu.SelectedRows)
+                {
+                    int dichVuIndex = row.Index;
+                    DichVu dichVu = DanhSachDichVu[dichVuIndex];
+                    dichVuDaChon.Add(dichVu);
+
+                   
+                    if (decimal.TryParse(dichVu.GiaTien.Replace("$", "").Replace(",", ""), out decimal giaTien))
+                    {
+                        tongTien += giaTien;
+                    }
+                }
+
+                // Hiển thị hóa đơn
+                StringBuilder hoaDon = new StringBuilder();
+                hoaDon.AppendLine("HÓA ĐƠN");
+                hoaDon.AppendLine($"Mã khách hàng: {khachHang.MaKhachHang}");
+                hoaDon.AppendLine($"Tên khách hàng: {khachHang.TenKhachHang}");
+                hoaDon.AppendLine("Dịch vụ đã chọn:");
+
+                foreach (var dichVu in dichVuDaChon)
+                {
+                    hoaDon.AppendLine($"- {dichVu.TenDichVu} - {dichVu.GiaTien}");
+                }
+
+                hoaDon.AppendLine($"Tổng tiền: {tongTien:C}");
+
+                MessageBox.Show(hoaDon.ToString(), "Hóa Đơn");
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn khách hàng và ít nhất một dịch vụ.");
+            }
+        }
+    }
+}
